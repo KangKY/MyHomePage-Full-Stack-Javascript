606420130a10c409cefb7a4a5591991b362e4d56
MSDN-SPECIAL\Administrator <ruddlf4933@gmail.com>
2017-07-12 23:15:32 +0900

    20170712 Heroku 배포를 위한 커밋

--- app_main.js
+++ app_main.js
@@ -36,6 +36,6 @@ app.all('/:route',function(req,res){
 
 /////////////////////////////////////////////////////////////
 
-app.listen(4000, function(){
-   console.log("Connected on port 4000!")
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
